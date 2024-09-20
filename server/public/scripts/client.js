042d6303ab5e1fd6b882621edb98d0713be500b8
Tom Pinataro <tompinataro@gmail.com>
2024-09-20 12:21:50 -0500

    minor adjustments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,196 @@
 console.log('JS is sourced!');
+getToDoList();  //calling the function upon DOM loading
+
+// * When the DOM loads, our `client.js` file will need 
+//to execute a function that makes an HTTP `GET /todos` request.
+
+function getToDoList() {
+    axios.get('/todos')
+      .then(response => {
+        let todoData = response.data;
+        console.log('This is the todo data we GET from server:', todoData);
+    
+        showToDoList(todoData);
+      })
+    
+      .catch((err) => {
+        console.log('Error in getting data!', err);
+      })
+    } // end of getToDoList
+
+
+
+// ===================== DISPLAY IN HTML FUNCTION =====================
+// When the data arrives client-side, need to loop through the 
+// array of objects and render a <tr> for each to-do item.
+// (Inside the <tbody>.)
+
+function showToDoList(data) {
+  console.log('This is the data to be displayed. Is this correct?:', data);
+  let viewTodos = document.getElementById('viewTodos');
+
+  //***** */ IF isComplete === true, 'Complete' button doesn't exist
+  //***** */ ELSE, 'Complete' button exists
+  viewTodos.innerHTML = '';
+  for(let task of data) { // for each object of the array
+    
+    console.log(`Is ${task.text} complete? ${task.isComplete}`);
+    if(task.isComplete === true) { //putting the idtag on line 40 took 1hr :/ 
+      viewTodos.innerHTML += `
+      <tr data-testid="toDoItem"" class="completed">  
+        <td>${task.text}</td>
+        <td>${task.isComplete}</td>
+        <td>Completed!</td>
+        <td><button data-testid="deleteButton" onClick="deleteTask(${task.id})">Delete Task</button></td>
+      </tr>`;
+    
+    } else {     
+      viewTodos.innerHTML += `
+      <tr data-testid="toDoItem">  
+        <td>${task.text}</td>
+        <td>${task.isComplete}</td>
+        <td><button data-testid="completeButton" onClick="completeTask(${task.id})">Complete Task?</button></td>
+        <td><button data-testid="deleteButton" onClick="deleteTask(${task.id})">Delete Task</button></td>
+      </tr>`;
+
+    } // end of IF statement
+    
+  } // end of FOR loop
+} // end of displayTodos
+
+// ===================== DELETE FUNCTION (Needs getTodos function in .then) =====================
+// Create an onclick delete <button> 
+
+function deleteTask(taskId) {
+  console.log ("In deleteTask()") 
+
+    console.log("This is the task's id in client:", taskId);
+    
+// N.B.: commented out the sweet alert 69-78 and 92-93 to pass tests*****************
+
+    // Swal.fire({
+    //   title: "Are you sure you want to delete?",
+    //   showDenyButton: true,
+    //   showCancelButton: false,
+    //   confirmButtonText: "Yes, Delete",
+    //   denyButtonText: `No, Do Not Delete`
+    // }).then((result) => {
+
+    //   if (result.isConfirmed) {
+    //     Swal.fire("Deleted", "", "success");
+        axios({
+          method: 'DELETE',
+          url: `/todos/${taskId}`
+        })
+
+        .then((response) => {
+          console.log(`${taskId} has been deleted:`, response);
+      
+          getToDoList()        })
+
+        .catch((err) => {
+          console.log(`${taskId} did not get deleted:`, err);
+        })
+    //   } else if (result.isDenied) {
+    //     Swal.fire("No Changes Made", "", "success");
+    //   }
+    // });
+  
+  }
+  // end of deleteTask()
+
+// ============ UPDATE COMPLETED STATUS ==================
+
+  function completeTask(task_id) {
+    console.log ("In completeTask()")
+
+    axios ({
+      method: 'PUT',
+      url: `/todos/${task_id}`,
+      data: {status: 'true'}
+    })
+    .then((response ) => {
+      getToDoList();
+    })  //end .then
+
+    .catch((error) => {
+      console.log('error updating Transfer status',error)
+    })
+  }
+// end of completeTask()
+
+// in the showToDoList()for each item in the to do list in client.js  
+// Using the onclick to make an http DELETE /todos request w/axios in client.js 
+// to server.js
+// Create an onclick completed button for each to do item in html table
+// Event makes an http request w/axios and .then to server (completed = TRUE)
+
+
+
+
+
+
+//     * This button must be a child of the element that has the `data-testid="toDoItem"` on it.
+// 6. Each completed to-do item must have:
+//     * A CSS class of `completed` applied to its `data-testid="toDoItem"` element.
+//     * Examples of "completed" to-do items:
+//       * ```js
+//           <tr data-testid="toDoItem" class="completed">...</tr>
+//           -->
+
+
+
+
+
+
+
+ 
+
+
+// ============ UPDATE TRANSFER STATUS ==================
+
+
+
+
+
+// ===================== BUTTON && POST FUNCTION =====================
+
+function addTask (event){
+// gather input info in variables
+  event.preventDefault();
+  console.log("Submit Button Clicked");
+  let task = document.getElementById('toDoTextInput').value;
+  console.log("Variables Match:", task);
+
+  //Put variables into object (creates object)
+let newTask = {
+  task: task
+}
+
+
+
+  console.log("Incoming Task:", newTask);
+
+  
+
+  //Send object to server.js using axios
+  axios({
+    method: 'POST',
+    url: '/todos',
+    data: newTask    
+  }).then(function(response) {
+    console.log(response.data);
+    document.getElementById('form').reset();
+
+    getToDoList();
+
+  }).catch(function(error) {
+    console.log('error in todosPOST', error); 
+    alert('Error adding todos object. Please try again.')       
+  });
+
+}
+
+
+
+
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,142 @@
-const router = require('express').Router();
+
+// 1. The todosRouter is created using the Express.js Router.
+
+const todosRouter = require('express').Router();
+
+// 2. A connection to the database is established using the pool = require
+// ('../modules/pool'); line.
 const pool = require('../modules/pool');
 
+// DB CONNECTION
 
 
+// In todos.router, make the router.get route.
+// (To get the to-do items!)
+// The route we just made will need to send a SELECT * FROM SQL query (using pool.query()!) to the database. 
+// NOTE: console.logs made here are displayed in terminal!!!!!!
 
-module.exports = router;
+// 3. The todosRouter.get('/', ...) route is defined to retrieve all todo items from the database. 
+// It sends a SELECT * FROM SQL query to the database using pool.query(). 
+// If the query is successful, it sends the resulting data back to the client. 
+// If there's an error, it logs the error and sends a 500 Internal Server Error response.
+
+// GET Todo data  GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_GET_
+todosRouter.get('/', (req, res) => {
+    let queryText = `
+    SELECT * FROM "todos"
+    `
+    pool.query(queryText)
+    .then(dbResult => {
+
+// Send the SQL query's resulting data back to the client.
+        res.send(dbResult.rows)
+    })
+    .catch(dbError => {
+        console.log('sql query error',dbError);
+        res.sendStatus(500);
+    })
+})
+
+
+// POST POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_POST_
+// (POST inserts a new row)
+
+// In todos.router, make the router.post route.
+// The route we just made will need to send an INSERT INTO SQL query (using pool.query!) to the database. (To insert a new row.)
+// If the INSERT INTO worked, send status 201 back to the client-side.
+// When client-side receives status 201, need to call the function that that gets the to-dos data and renders it.
+
+4.
+//The todosRouter.post('/', ...) route is defined to insert a new todo item into the database. 
+// It sends an INSERT INTO SQL query to the database using pool.query(). 
+// If the insertion is successful, it sends a 200 OK response. 
+// If there's an error, it logs the error and sends a 500 Internal Server Error response.
+
+todosRouter.post('/', (req, res) => {
+    console.log('POST received a request!');
+    console.log('\treq.body is:', req.body);
+    console.log('This is the req.body.text:', req.body.task);
+  
+    const sqlText = `
+      INSERT INTO "todos" 
+        ("text", "isComplete") 
+        VALUES
+        ($1, $2); 
+    `;  
+    const sqlValues = [ 
+      req.body.task,
+      false
+    ];
+    pool.query(sqlText, sqlValues)
+      .then((dbResult) => {
+        res.sendStatus(200);
+      })
+      .catch((dbErr) => {
+        console.log('SQL query in POST /todos error:', dbErr);
+        res.sendStatus(500);
+      })
+    
+  });
+
+
+// PUT - Route is defined to mark a todo item as completed. 
+
+// 5. The todosRouter.put('/:task_id', ...)
+// It sends an UPDATE SQL query to the database using pool.query(). 
+// If the update is successful, it sends a 200 OK response. 
+// If there's an error, it logs the error and sends a 500 Internal Server Error response.
+
+
+// PUT_PUT_PUT_PUT_PUT_PUTPUT_PUT_PUT_PUT_PUT_PUTPUT_PUT_PUT_PUT_PUT_PUTPUT_PUT_PUT_PUT_PUT_PUT
+todosRouter.put ('/:task_id', (req, res) => {
+    const todoToChange = req.params.task_id;
+    
+    const sqlText = `
+        UPDATE "todos"
+            SET "isComplete" = TRUE
+            WHERE "id" = $1;
+            `
+    const sqlValues = [todoToChange]
+    pool.query(sqlText, sqlValues)
+    .then(dbResult => {
+        res.sendStatus(200);
+    })
+    .catch(dbError => {
+        console.log('db error updating status',dbError);
+        res.sendStatus(500);
+    })
+})
+
+
+// In todos.router, make the router.delete route
+// The route we just made will need to send a DELETE SQL query (using pool query!) to the database. (To delete a task/row.)
+// If the DELETE was successful, send status 202 back to client.js
+// When client.js receives status 202, call the function that gets the remaining to-dos data and renders it
+
+// 6. The todosRouter.delete('/:id', ...) route is defined to delete a todo item from the database. 
+// It sends a DELETE SQL query to the database using pool.query(). 
+// If the deletion is successful, it sends a 200 OK response. 
+// If there's an error, it logs the error and sends a 500 Internal Server Error response.
+
+// DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_DELETE_
+todosRouter.delete('/:id', (req, res) => {
+
+    const todosId = req.params.id;
+    console.log('The id from DELETE request:', todosId);
+    
+    const sqlText = `DELETE FROM "todos"
+                    WHERE "id" = $1;`;
+
+    const sqlValue = [todosId];
+
+    pool.query(sqlText, sqlValue)
+    .then(dbResult => {
+        res.sendStatus(200);
+    })
+    .catch(dbError => {
+        res.sendStatus(500);
+    })
+
+})
+// 7. The todosRouter is exported as a module using module.exports = todosRouter;.
+module.exports = todosRouter;
